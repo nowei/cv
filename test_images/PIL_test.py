817c2db728540b51b112ca4ced3f254fb05106cf
Andrew Wei <andrewwei1999@gmail.com>
2019-09-05 15:27:45 +0800

    adding script that adds alpha channel

--- test_images/PIL_test.py
+++ test_images/PIL_test.py
@@ -1,11 +1,13 @@
 from PIL import Image
 im = Image.open('lynnwood.jpg')
 size = im.size
-pix = im.load()
+imOut = im.convert("RGBA")
+
+pix = imOut.load()
 print(pix[1,1])
 for x in range(size[0]):
     for y in range(size[1]):
         c = pix[x,y]
-        pix[x,y] = (c[0],c[1],c[2]//2,255)
-im.save('lynnhood.jpg')
+        pix[x,y] = (c[0],c[1],c[2]//2,120)
+imOut.save('lynnhood.png')
 
